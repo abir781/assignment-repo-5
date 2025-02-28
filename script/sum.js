d9bcb97ce0268c74e92a13262b29fd6d3df757e5
abir781 <hasan.abir3176@gmail.com>
2025-03-01 02:17:33 +0600

    added-js

---
+++ script/sum.js
@@ -0,0 +1,137 @@
+document.getElementById("id-1").addEventListener("click",function(){
+    alert("Board updated Successfully")
+    const amount=document.getElementById("minus").innerText;
+    let convertedamount=parseInt(amount);
+    convertedamount=convertedamount-1;
+    
+    document.getElementById("minus").innerText=convertedamount;
+
+    const amount2=document.getElementById("plus").innerText;
+    let convertedamount2=parseInt(amount2);
+    convertedamount2=convertedamount2+1;
+    document.getElementById("plus").innerText=convertedamount2;
+
+    const btn=document.getElementById("id-1");
+    btn.setAttribute('disabled',true);
+    btn.classList.remove("bg-[#3752FD]");
+    btn.classList.add("bg-[#CED6FF]");
+
+    if(convertedamount===0){
+        alert("congrats!!! You have completed all the current task");
+    }
+})
+
+document.getElementById("id-2").addEventListener("click",function(){
+    alert("Board updated Successfully")
+    const amount=document.getElementById("minus").innerText;
+    let convertedamount=parseInt(amount);
+    convertedamount=convertedamount-1;
+    
+    document.getElementById("minus").innerText=convertedamount;
+
+    const amount2=document.getElementById("plus").innerText;
+    let convertedamount2=parseInt(amount2);
+    convertedamount2=convertedamount2+1;
+    document.getElementById("plus").innerText=convertedamount2;
+
+    const btn=document.getElementById("id-2");
+    btn.setAttribute('disabled',true);
+    btn.classList.remove("bg-[#3752FD]");
+    btn.classList.add("bg-[#CED6FF]");
+
+    if(convertedamount===0){
+        alert("congrats!!! You have completed all the current task");
+    }
+})
+
+document.getElementById("id-3").addEventListener("click",function(){
+    alert("Board updated Successfully")
+    const amount=document.getElementById("minus").innerText;
+    let convertedamount=parseInt(amount);
+    convertedamount=convertedamount-1;
+    
+    document.getElementById("minus").innerText=convertedamount;
+
+    const amount2=document.getElementById("plus").innerText;
+    let convertedamount2=parseInt(amount2);
+    convertedamount2=convertedamount2+1;
+    document.getElementById("plus").innerText=convertedamount2;
+
+    const btn=document.getElementById("id-3");
+    btn.setAttribute('disabled',true);
+    btn.classList.remove("bg-[#3752FD]");
+    btn.classList.add("bg-[#CED6FF]");
+
+    if(convertedamount===0){
+        alert("congrats!!! You have completed all the current task");
+    }
+})
+
+document.getElementById("id-4").addEventListener("click",function(){
+    alert("Board updated Successfully")
+    const amount=document.getElementById("minus").innerText;
+    let convertedamount=parseInt(amount);
+    convertedamount=convertedamount-1;
+    
+    document.getElementById("minus").innerText=convertedamount;
+
+    const amount2=document.getElementById("plus").innerText;
+    let convertedamount2=parseInt(amount2);
+    convertedamount2=convertedamount2+1;
+    document.getElementById("plus").innerText=convertedamount2;
+
+    const btn=document.getElementById("id-4");
+    btn.setAttribute('disabled',true);
+    btn.classList.remove("bg-[#3752FD]");
+    btn.classList.add("bg-[#CED6FF]");
+
+    if(convertedamount===0){
+        alert("congrats!!! You have completed all the current task");
+    }
+})
+
+document.getElementById("id-5").addEventListener("click",function(){
+    alert("Board updated Successfully")
+    const amount=document.getElementById("minus").innerText;
+    let convertedamount=parseInt(amount);
+    convertedamount=convertedamount-1;
+    
+    document.getElementById("minus").innerText=convertedamount;
+
+    const amount2=document.getElementById("plus").innerText;
+    let convertedamount2=parseInt(amount2);
+    convertedamount2=convertedamount2+1;
+    document.getElementById("plus").innerText=convertedamount2;
+
+    const btn=document.getElementById("id-5");
+    btn.setAttribute('disabled',true);
+    btn.classList.remove("bg-[#3752FD]");
+    btn.classList.add("bg-[#CED6FF]");
+
+    if(convertedamount===0){
+        alert("congrats!!! You have completed all the current task");
+    }
+})
+
+document.getElementById("id-6").addEventListener("click",function(){
+    alert("Board updated Successfully")
+    const amount=document.getElementById("minus").innerText;
+    let convertedamount=parseInt(amount);
+    convertedamount=convertedamount-1;
+    
+    document.getElementById("minus").innerText=convertedamount;
+
+    const amount2=document.getElementById("plus").innerText;
+    let convertedamount2=parseInt(amount2);
+    convertedamount2=convertedamount2+1;
+    document.getElementById("plus").innerText=convertedamount2;
+
+    const btn=document.getElementById("id-6");
+    btn.setAttribute('disabled',true);
+    btn.classList.remove("bg-[#3752FD]");
+    btn.classList.add("bg-[#CED6FF]");
+
+    if(convertedamount===0){
+        alert("congrats!!! You have completed all the current task");
+    }
+})
