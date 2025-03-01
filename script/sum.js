ebbe2bc545c20d42459cb2c060622f1d4a31c36b
abir781 <hasan.abir3176@gmail.com>
2025-03-01 10:58:38 +0600

    added-page

--- script/sum.js
+++ script/sum.js
@@ -204,3 +204,11 @@ document.getElementById("id-6").addEventListener("click",function(){
 document.getElementById("clear-history").addEventListener("click",function(){
     document.getElementById("show").innerHTML="";
 })
+
+document.getElementById("color-change").addEventListener("click",function(){
+    document.body.style.backgroundColor='rgb('+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+')';
+})
+
+document.getElementById("page").addEventListener("click",function(){
+    window.location.href="./answer.html";
+})
